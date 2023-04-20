9fcc5b1a848103462d5fac74c50dd4fcaa8e548c
Adityakitukale49 <adityakitukale4599@gmail.com>
2023-04-20 19:13:52 +0530

    My-Portfolio

--- app.js
+++ app.js
@@ -23,3 +23,19 @@ menu_item.forEach((item) => {
 		mobile_menu.classList.toggle('active');
 	});
 });
+const scriptURL = 'https://script.google.com/macros/s/AKfycbza_UH8zcdrh4rYKixUzGsTuo-s30_wpk1IIYzjsZ0Osm3eOfrMhrPBsp8RmncKfYqM/exec'
+    const form = document.forms['submit-to-google-sheet']
+     const msg = document.getElementById("msg")
+  
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => {
+            msg.innerHTML = "Message sent Successfully"
+            setTimeout(function(){
+                msg.innerHTML = ""
+            },5000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+    })
